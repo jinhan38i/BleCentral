dfd4fab1fec75f84e05e6e7e4be41d6b4b2503bc
jinhankim <jinhan38@pood.pet>
2023-12-27 18:36:40 +0900

    init

---
+++ inhand/CameraDetection/app/src/main/java/com/example/cameradetection/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.example.cameradetection
+
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import androidx.activity.ComponentActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+
+private val PERMISSIONS_REQUIRED = arrayOf(Manifest.permission.CAMERA)
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        Log.d(TAG, "onCreate: checkPermission : ${checkPermission(this)}")
+        if (!checkPermission(this)) {
+            ActivityCompat.requestPermissions(this, PERMISSIONS_REQUIRED, 100)
+        }
+
+        findViewById<Button>(R.id.move_button).setOnClickListener {
+            if (checkPermission(this)) {
+                Log.d(TAG, "onCreate: 권한 있음")
+            } else {
+                ActivityCompat.requestPermissions(this, PERMISSIONS_REQUIRED, 100)
+            }
+        }
+    }
+
+
+    private fun checkPermission(context: Context) = PERMISSIONS_REQUIRED.all {
+        ContextCompat.checkSelfPermission(
+            context,
+            it
+        ) == PackageManager.PERMISSION_GRANTED
+    }
+
+
+    companion object {
+        private const val TAG = "MainActivity"
+    }
+}
