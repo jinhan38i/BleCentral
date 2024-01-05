f2d05919185edf2503ea4613a27562a20122f144
jinhankim <jinhan38@pood.pet>
2024-01-05 20:50:13 +0900

    추가

---
+++ inhand/BleCentral/app/src/main/java/com/example/bleCentral/MainActivity.kt
@@ -0,0 +1,67 @@
+package com.example.bleCentral
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import com.example.bleCentral.ble.BleUtil
+import com.example.bleCentral.ble.BleUuid
+import com.example.blecentral.R
+
+class MainActivity : AppCompatActivity() {
+
+    companion object {
+        private const val TAG = "MainActivity"
+    }
+
+    @SuppressLint("SetTextI18n")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val uuid = BleUuid(
+            serviceUuid = "fec26ec4-6d71-4442-9f81-55bc21d658d0",
+            charUuid = "fec26ec4-6d71-4442-9f81-55bc21d658d1",
+            descriptorUuid = "00002902-0000-1000-8000-00805f9b34fb",
+        )
+        BleUtil.setInstance(this, uuid)
+        findViewById<Button>(R.id.bt_scan).setOnClickListener {
+            if (!BleUtil.checkBlePermission(this)) return@setOnClickListener
+            startActivity(Intent(this, CentralActivity::class.java))
+        }
+        findViewById<Button>(R.id.bt_peripheral).setOnClickListener {
+            if (!BleUtil.checkBlePermission(this)) return@setOnClickListener
+            startActivity(Intent(this, PeripheralActivity::class.java))
+        }
+        findViewById<Button>(R.id.bt_ble_enable).setOnClickListener {
+            BleUtil.changeBluetoothEnable(this)
+            findViewById<Button>(R.id.bt_ble_enable).text = "Enable = ${BleUtil.getBleEnable()}"
+        }
+
+        findViewById<Button>(R.id.bt_ble_enable).text = "Enable = ${BleUtil.getBleEnable()}"
+        BleUtil.requestNotificationPermission(this)
+
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        Log.d(
+            TAG,
+            "onRequestPermissionsResult() called with: requestCode = $requestCode, permissions = $permissions, grantResults = $grantResults"
+        )
+    }
+
+    @Deprecated("Deprecated in Java")
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        Log.d(
+            TAG,
+            "onActivityResult() called with: requestCode = $requestCode, resultCode = $resultCode, data = $data"
+        )
+    }
+}
